1432e896381f1bb5bbecf6e8f97cad14ab52b06a
Foster Mbayi <mbayifoster@gmail.com>
2025-01-27 11:19:32 +0100

    first commit

---
+++ resources/js/App.vue
@@ -0,0 +1,22 @@
+<template>
+    <div id="app">
+      <h1>Bienvenue dans mon application Vue.js</h1>
+      <MonComposant titre="Titre du Composant" description="Ceci est une description personnalisée." />
+      <MonComposant titre="Un autre Titre" />
+    </div>
+  </template>
+  
+  <script>
+  import MonComposant from './src/components/MonComposant.vue';
+  
+  export default {
+    name: 'App',
+    components: {
+      MonComposant
+    }
+  }
+  </script>
+  
+  <style>
+  /* Styles globaux ici */
+  </style>
